e3be403eb490f9f5394ff1cb33349d6113b7f3be
buzzbombnc <buzzbomb-github@buzzbomb.net>
2017-04-06 09:33:47 -0400

    Authentication via firebaseui.

--- src/Authentication.js
+++ src/Authentication.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 
+var firebaseui = require("firebaseui");
+
 export default class Authentication extends Component {
+    constructor(props) {
+        super(props);
+
+        var uiconfig = {
+            callbacks: {
+                signInSuccess: () => false,
+            },
+            signInOptions: [
+                this.props.firebase.auth.EmailAuthProvider.PROVIDER_ID
+            ],
+            tosUrl: ''
+        };
+        var ui = new firebaseui.auth.AuthUI(this.props.firebase.auth());
+        ui.start("#auth-container", uiconfig);
+    }
+
     render() {
         return (
-            <div>Authentication</div>
+            <div id="auth-container">Authentication</div>
         );
     }
 }
