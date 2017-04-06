27ea22d1ee1681590556fac5f2f6eb034e3e5fac
buzzbombnc <buzzbomb-github@buzzbomb.net>
2017-04-06 18:39:05 -0400

    Add (un)like button and hook it up to the database.

--- src/PostLike.js
+++ src/PostLike.js
@@ -15,6 +15,8 @@ export default class PostLike extends Component {
         this.likesRef = firebase.database().ref('posts').child(this.props.postkey).child('likes');
         this.componentWillMount = this.componentWillMount.bind(this);
         this.componentWillUnmount = this.componentWillUnmount.bind(this);
+        this.like = this.like.bind(this);
+        this.unlike = this.unlike.bind(this);
     }
 
     componentWillMount() {
@@ -36,12 +38,29 @@ export default class PostLike extends Component {
         this.likesRef.off();
     }
 
+    like() {
+        var userid = firebase.auth().currentUser.uid;
+        this.likesRef.set({ [userid]: true });
+    }
+
+    unlike() {
+        var userid = firebase.auth().currentUser.uid;
+        this.likesRef.child(userid).remove();
+    }
+
     render() {
         var userid = firebase.auth().currentUser.uid;
         var userlikes = this.state.likes.includes(userid);
+
+        var button;
+        if (userlikes) {
+            button = (<button onClick={this.unlike}>Unlike</button>);
+        } else {
+            button = (<button onClick={this.like}>Like</button>);
+        }
         return (
             <div className="Postlikes">
-                <p>{ String(userlikes) } { this.state.likes.length }</p>
+                <p>{ button } { this.state.likes.length }</p>
             </div>
         );
     }
