30cc6db13ca0caeb09c0bc55cf4d68f3ba8e2b4c
buzzbombnc <buzzbomb-github@buzzbomb.net>
2017-04-06 10:44:04 -0400

    Basic Post display from Firebase.

--- src/Post.js
+++ src/Post.js
@@ -3,8 +3,13 @@ import './App.css';
 
 export default class Post extends Component {
     render() {
+        var post = this.props.post;
         return (
-            <div>Post</div>
+            <div class="Post">
+                <h1>{ post.user }</h1>
+                <p>{ post.message }</p>
+                <p>{ String(post.likes) }</p>
+            </div>
         );
     }
 }
