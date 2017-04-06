05529e1e104f81ae44781b545148a6df37ae94c7
buzzbombnc <buzzbomb-github@buzzbomb.net>
2017-04-06 10:36:57 -0400

    Create and populate Post components.

---
+++ src/Post.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class Post extends Component {
+    render() {
+        return (
+            <div>Post</div>
+        );
+    }
+}
--- src/PostList.js
+++ src/PostList.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
 
+import Post from './Post';
+
 var reactfire = require('reactfire');
 
 // Use non-ES6 creation to enable the use of mixins.
 
 var PostList = React.createClass({
-    render: function() { return (
-                <div>PostList</div>
-                );
-            },
+    render: function() {
+        return (
+            <div id="postlist">
+                { this.state.posts.map((p) =>
+                    <Post key={p['.key']} post={p} />
+                )}
+            </div>);
+    },
     mixins: [reactfire],
     componentWillMount: function() {
         var ref = this.props.firebase.database().ref("posts");
