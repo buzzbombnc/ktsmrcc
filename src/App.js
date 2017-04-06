5ea56b4c40a1510db29a0f646edac0044672dd55
buzzbombnc <buzzbomb-github@buzzbomb.net>
2017-04-06 09:05:20 -0400

    Built Authentication component shell.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Authentication from './Authentication';
+
 // Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDNjGyWHZHgPJ2sQBo8_3J76qPeOc3EWLU",
@@ -23,6 +25,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Authentication />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
---
+++ src/Authentication.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class Authentication extends Component {
+    render() {
+        return (
+            <div>Authentication</div>
+        );
+    }
+}
