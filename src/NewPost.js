84bc3a6ff0db7a9266be1c8de9975b53590595fa
buzzbombnc <buzzbomb-github@buzzbomb.net>
2017-04-06 11:43:57 -0400

    Adding new posts works!

--- src/NewPost.js
+++ src/NewPost.js
@@ -9,16 +9,33 @@ export default class NewPost extends Component {
             message: ''
         };
 
+        this.postsref = this.props.firebase.database().ref('posts');
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
         this.setState({ message: event.target.value});
     }
 
+    handleSubmit(event) {
+        // Build the post.
+        var postdata = {
+            user: this.props.firebase.auth().currentUser.email,
+            message: this.state.message,
+            likes: {}
+        };
+        // Get a new key for this post.
+        var newPostRef = this.postsref.push();
+        newPostRef.set(postdata);
+        // Clean up for a new new post!
+        this.setState({message: ''});
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={ this.handleSubmit }>
                 <label>
                     Message:
                     <textarea value={ this.state.message} onChange={ this.handleChange } />
