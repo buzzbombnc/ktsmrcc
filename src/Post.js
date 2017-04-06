450d51cec349dd245d987c7e52f375617fd1007c
buzzbombnc <buzzbomb-github@buzzbomb.net>
2017-04-06 11:43:31 -0400

    Change div property to "className".

--- src/Post.js
+++ src/Post.js
@@ -5,7 +5,7 @@ export default class Post extends Component {
     render() {
         var post = this.props.post;
         return (
-            <div class="Post">
+            <div className="Post">
                 <h1>{ post.user }</h1>
                 <p>{ post.message }</p>
                 <p>{ String(post.likes) }</p>
